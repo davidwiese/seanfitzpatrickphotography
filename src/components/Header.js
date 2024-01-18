2aa8001eb80473dbe0219562715ffd2e3b2363cb
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-01-18 15:45:00 -0800

    Add more work on header/mobile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,43 @@ import { Link } from "react-router-dom";
 const Header = () => {
 	return (
 		<header className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
-			Header
+			<div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+				{/* Logo */}
+				<Link to={"/"} className="max-w-[200px]">
+					<img src={Logo} alt="homepage" />
+				</Link>
+				{/* nav - initially hidden - show on desktop */}
+				<nav className="hidden lg:flex gap-12 font-semibold">
+					<Link
+						to={"/"}
+						className="text-[#696c6d] hover:text-primary transition"
+					>
+						Home
+					</Link>
+					<Link
+						to={"/about"}
+						className="text-[#696c6d] hover:text-primary transition"
+					>
+						About
+					</Link>
+					<Link
+						to={"/portfolio"}
+						className="text-[#696c6d] hover:text-primary transition"
+					>
+						Portfolio
+					</Link>
+					<Link
+						to={"/contact"}
+						className="text-[#696c6d] hover:text-primary transition"
+					>
+						Contact
+					</Link>
+				</nav>
+			</div>
+			{/* socials */}
+			<Socials />
+			{/* mobile nav */}
+			<MobileNav />
 		</header>
 	);
 };
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { CgMenuRight } from "react-icons/cg";
+import Link from "react-router-dom";
+import { motion } from "framer-motion";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+	return (
+		<nav>
+			{/* nav open button */}
+			<div className="text-3xl cursor-pointer">
+				<CgMenuRight />
+			</div>
+			{/* menu */}
+			<div className="bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20">
+				Menu
+			</div>
+		</nav>
+	);
 };
 
 export default MobileNav;
