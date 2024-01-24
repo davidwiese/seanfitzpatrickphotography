e7f34e87043e9424085167e8d1d01906f5bd355f
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-01-24 11:14:24 -0800

    Add mouse cursor context to About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import WomanImg from "../img/about/woman.png";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { transition1 } from "../transitions";
+import { CursorContext } from "../context/CursorContext";
 
 const About = () => {
+	const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
 	return (
 		<motion.section
 			initial={{ opacity: 0, y: "100%" }}
@@ -13,7 +16,11 @@ const About = () => {
 			transition={transition1}
 			className="section "
 		>
-			<div className="container mx-auto h-full relative">
+			<div
+				onMouseEnter={mouseEnterHandler}
+				onMouseLeave={mouseLeaveHandler}
+				className="container mx-auto h-full relative"
+			>
 				{/* text & img */}
 				<div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
 					{/* image */}
