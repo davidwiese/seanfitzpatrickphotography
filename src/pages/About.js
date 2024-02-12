7288ce5c92bb3e688cbde45a7857311234cd8b1c
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-02-12 13:44:43 -0800

    Adjust about page placement

--- src/pages/About.js
+++ src/pages/About.js
@@ -24,7 +24,7 @@ const About = () => {
 				{/* text & img */}
 				<div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
 					{/* image */}
-					<div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden mt-[-40px]">
+					<div className="flex-1 max-h-96 lg:max-h-max md:max-h-max order-2 lg:order-none overflow-hidden mt-[-40px]">
 						<img src={ManImg} alt="" />
 					</div>
 					{/* text */}
@@ -33,7 +33,7 @@ const About = () => {
 						animate={{ opacity: 1, y: 0 }}
 						exit={{ opacity: 0, y: "-80%" }}
 						transition={transition1}
-						className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start "
+						className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
 					>
 						<h1 className="h1">About Me</h1>
 						<p className="mb-12 max-w-sm">
